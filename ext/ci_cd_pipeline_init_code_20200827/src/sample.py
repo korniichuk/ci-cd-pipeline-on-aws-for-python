ca24aa19f58f6ba3413067872f20b7aa7df39401
Ruslan Korniichuk <ruslan.korniichuk@capgemini.com>
2020-08-27 18:00:45 +0200

    Add init code for new AWS CodeCommit repo

---
+++ ext/ci_cd_pipeline_init_code_20200827/src/sample.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Name: sample
+# Version: 0.1a1
+# Owner: Ruslan Korniichuk
+
+def hello_world():
+    return 'Hello, World!'
---
+++ ext/ci_cd_pipeline_init_code_20200827/tests/test_sample.py
@@ -0,0 +1,5 @@
+from src.sample import hello_world
+
+
+def test_hello_world():
+    assert hello_world() == 'Hello, World!'
